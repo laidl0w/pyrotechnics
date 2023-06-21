364e5f7125346a42786b93a3b3ceda8239024c0d
Laidlow <twinkladova@gmail.com>
2023-06-22 00:21:16 +0200

    Initial commit

---
+++ src/main/java/net/laid/pyrotechnics/custom/WorkshopBlock.java
@@ -0,0 +1,10 @@
+package net.laid.pyrotechnics.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class WorkshopBlock extends Block {
+    public WorkshopBlock(Properties properties) {
+        super(properties);
+    }
+
+}
